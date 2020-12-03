42b0413df1ad734e3ac5cae26910fad6f3be2180
pobrebski <pobrebski@microstrategy.com>
2020-12-03 13:53:20 +0100

    Add simple layout.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      </header>
+      <main className="App-main">
+        <aside className="App-main-aside-left">
+        </aside>
+        <aside className="App-main-aside-right">
+        </aside>
+        <div className="App-main-header">
+          <div className="App-main-header-animation">
+
+          </div>
+        </div>
+        <div className="App-main-body">
+          <div class="grid-container">
+            <div class="Pracownicy"> Pracownicy</div>
+            <div class="im1">Jan</div>
+            <div class="naz1">Kowalski</div>
+            <div class="im2"></div>
+            <div class="naz2"></div>
+            <div class="im3"></div>
+            <div class="naz3"></div>
+            <div class="im4"></div>
+            <div class="naz4"></div>
+          </div>
+        </div>
+      </main>
+      <header className="App-footer">
       </header>
     </div>
   );
